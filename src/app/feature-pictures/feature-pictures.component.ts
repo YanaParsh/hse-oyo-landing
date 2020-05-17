4bd93abec11a5b6651333f11e142aa7147e0d64c
Yana Parshina <yana.parshinaRN@gmail.com>
2020-05-17 16:17:46 +0300

    add responsive layout

--- src/app/feature-pictures/feature-pictures.component.ts
+++ src/app/feature-pictures/feature-pictures.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-feature-pictures',
@@ -6,10 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./feature-pictures.component.scss']
 })
 export class FeaturePicturesComponent {
-  public readonly imagesUrls: string[] = [
+  constructor(private readonly domSanitizer: DomSanitizer) {}
+
+  public readonly imagesUrls: SafeStyle[] = [
     'assets/at-home.jpg',
-    'assets/at-office.jpg',
     'assets/at-travel.jpg',
+    'assets/at-office.jpg',
     'assets/outdoors.jpg'
-  ];
+  ].map((unsafeUrl) => this.domSanitizer.bypassSecurityTrustStyle(`url(${unsafeUrl})`));
 }
